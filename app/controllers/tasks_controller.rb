7c0fdbdf28777c4489503b4c119698770e9c8e2a
amy huang <flower850603@gmail.com>
2024-12-23 20:47:48 +0800

    create a new data and view all data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,27 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def details
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # create new database record with the form data
+    task = Task.new(task_params)
+    task.save
+    # redirect to the show page
+    redirect_to task_path(task)
+  end
+
+  private
+  # only allow the permitted parameters to be used in the create action
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
