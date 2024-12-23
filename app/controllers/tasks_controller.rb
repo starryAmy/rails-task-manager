8c78b6bea6ab4dfc56e24c40b6c40fcc0069487a
amy huang <flower850603@gmail.com>
2024-12-23 21:03:42 +0800

    users can update and delete items

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,9 +19,33 @@ class TasksController < ApplicationController
     redirect_to task_path(task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    # find the record to update
+    task = Task.find(params[:id])
+    # update the record with the form data
+    task.update(task_params)
+    # redirect to the show page
+    redirect_to task_path(task)
+  end
+
+  def destroy
+    # find the record to delete
+    @task = Task.find(params[:id])
+    # delete the record
+    @task.destroy
+    # redirect to the index page
+    # 這通常用於指示一個 POST 或 DELETE 操作之後，應該使用 GET 請求重定向到某個 URL。使用 303 可以確保用戶在刪除後不會重複提交同樣的刪除請求，並且會重定向到任務列表頁面。
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
   # only allow the permitted parameters to be used in the create action
   def task_params
     params.require(:task).permit(:title, :details)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "/tasks/new", to: "tasks#new", as: "new_task"
   post "/tasks", to: "tasks#create"
   get "/tasks/:id", to: "tasks#details", as: "task"
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#destroy"
 end
